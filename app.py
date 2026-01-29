e239676db9e597a039c992baed41f2cd2d70383e
arebecadev23 <rebecaavilla4@gmai.com>
2026-01-28 23:03:05 -0300

    alteração

--- app.py
+++ app.py
@@ -68,5 +68,8 @@ def jogar():
                            tamanho=session["tamanho_grid"])
 
 # --- ESSA PARTE É A QUE ESTAVA FALTANDO ---
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
